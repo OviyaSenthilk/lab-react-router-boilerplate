d950e76496401a6489661d9cd07f1de484978f30
OviyaSenthilk <pji.oviya.s@kalvium.community>
2024-06-24 18:37:56 +0530

    new

---
+++ router/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Contacts from './Pages/Contacts';
+import './App.css';
+
+
+const App = () => {
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contacts" element={<Contacts />} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default App;
